ee177fd67374b868f51d2885c102c4cd8445141a
Kimuji Akermi <akermiji@gmail.com>
2024-03-18 01:00:57 +0700

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-// src/App.js
+// // src/App.js
+// import React from 'react';
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import CountdownPage from './Countdown';
+// import MapPage from './Map';
+// import WebPage from './Web';
+// // import InvitePage from './Invite';
+// import './App.css';
+
+// function App() {
+//   return (
+//     // <div className="App">
+//     // <header className="App-header">
+//     <Router basename="/save-the-date">
+//     <Routes>
+//       <Route path="/" element={<CountdownPage />} />
+//       <Route path="/map" element={<MapPage />} />
+//       <Route path="/web" element={<WebPage />} />
+//       {/* <Route path="/countdown" element={<InvitePage />} /> */}
+//       {/* <Route path="*" element={<NotFound />} /> */}
+//       </Routes>
+//     </Router>
+//     //   </header>
+//     // </div>
+//   );
+// }
+
+// export default App;
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import CountdownPage from './Countdown';
 import MapPage from './Map';
 import WebPage from './Web';
-// import InvitePage from './Invite';
 import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    // <header className="App-header">
-    <Router basename="/save-the-date">
-    <Routes>
-      <Route path="/" element={<CountdownPage />} />
-      <Route path="/map" element={<MapPage />} />
-      <Route path="/web" element={<WebPage />} />
-      {/* <Route path="/countdown" element={<InvitePage />} /> */}
-      {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<CountdownPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/web" element={<WebPage />} />
+        </Routes>
+      </div>
     </Router>
-    //   </header>
-    // </div>
   );
 }
 
 export default App;
+
